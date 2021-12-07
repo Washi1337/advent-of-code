28f4e6495e53dcc82b77e619d37016692d83a59f
Washi <washi1337@protonmail.com>
2021-12-07 16:45:26 +0100

    Improved perf for 2021 day 07

--- 2021/day07/src/main.rs
+++ 2021/day07/src/main.rs
@@ -24,50 +24,48 @@ pub fn parse_input(file: &str) -> std::io::Result<Input> {
     })
 }
 
-fn get_minimum_fuel_short_circuit(input: &Input, fuel_cost: fn(isize) -> isize) -> isize {    
-    let max_pos = *input.positions.iter().max().unwrap();
+fn get_minimum_fuel_binary(input: &Input, fuel_cost: fn(isize) -> isize) -> isize {
 
-    let mut best: isize = isize::MAX;
-
-    for dest in 0..max_pos {
-        let mut total = 0;
-
-        for &pos in input.positions.iter() {
-            total += fuel_cost((pos - dest).abs());
-            if total >= best {
-                total = best;
-                break
-            }
-        }
-
-        best = total;
+    fn get_total_cost(input: &Input, dest: isize, fuel_cost: fn(isize) -> isize) -> isize {
+        input
+            .positions
+            .iter()
+            .map(|&pos| fuel_cost((pos - dest).abs()))
+            .sum()
     }
 
-    best
-}
+    // Key observation is that if you'd plot the total cost based on position, then you
+    // get a graph where the only local minimum == the global minimum (a sink).
+    //
+    //  => We can do something similar to a binary search. Start in the middle, and check 
+    //     by going left and right of the current candidate position which direction will 
+    //     decrease the total cost. Stop when both will result in an increase.
 
-fn get_minimum_fuel_naive(input: &Input, fuel_cost: fn(isize) -> isize) -> isize {
-    let max_pos = *input.positions.iter().max().unwrap();
+    let mut mid_pos = input.positions.iter().sum::<isize>() / input.positions.len() as isize;
+    let mut mid_fuel = get_total_cost(&input, mid_pos, fuel_cost);
 
-    (0..max_pos)
-        .map(|dest|{
-            let total_fuel: isize = input
-                .positions
-                .iter()
-                .map(|&pos| fuel_cost((pos - dest).abs()))
-                .sum();
-            total_fuel
-        })
-        .min()
-        .unwrap()
+    loop {
+        let left_fuel = get_total_cost(&input, mid_pos - 1, fuel_cost);
+        let right_fuel = get_total_cost(&input, mid_pos + 1, fuel_cost);
+
+        if left_fuel < mid_fuel {
+            mid_fuel = left_fuel;
+            mid_pos -= 1;
+        } else if right_fuel < mid_fuel {
+            mid_fuel = right_fuel;
+            mid_pos += 1;
+        } else {
+            return mid_fuel;
+        }
+    }
 }
 
 pub fn part1(input: &Input) -> isize {
-    get_minimum_fuel_short_circuit(&input, |distance| distance)
+    get_minimum_fuel_binary(&input, |distance| distance)
 }
 
 pub fn part2(input: &Input) -> isize {
-    get_minimum_fuel_short_circuit(&input, |distance| distance * (distance + 1) / 2)
+    get_minimum_fuel_binary(&input, |distance| distance * (distance + 1) / 2)
 }
 
 fn main() -> std::io::Result<()> {
@@ -89,6 +87,6 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-// Parse: (time: 107us)
-// Solution 1: 348996 (time: 487us)
-// Solution 2: 98231647 (time: 1786us)
+// Parse: (time: 117us)
+// Solution 1: 348996 (time: 69us)
+// Solution 2: 98231647 (time: 5us)
